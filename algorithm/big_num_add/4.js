cf56d0eac293b2e295902216817635ad17831982
WildBlue58 <2398205893@qq.com>
2025-05-29 18:58:29 +0800

    新增algorithm/big_num_add/4.js文件，演示BigInt的声明方式及其与普通数字的相加操作，旨在加深对JavaScript大数处理的理解。

---
+++ algorithm/big_num_add/4.js
@@ -0,0 +1,7 @@
+const bigNum = 123456789012345678901234567890123456789n
+// BigInt 申明方式 函数申明
+// BigInt 简单数据类型,不是对象,不是构造函数
+const theNum = new BigInt("123456789012345678901234567890123456789")// 函数的方式 不用字符串不准确
+console.log(bigNum, theNum,typeof bigNum,typeof 1);
+// console.log(bigNum + 1);// 不能够混合相加
+console.log(bigNum + 1n);
