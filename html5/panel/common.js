b4717b25e4c76230fb3ea164ada2d16dd0480d20
WildBlue <2398205893@qq.com>
2025-06-02 17:22:04 +0800

    为common.js文件添加获取所有面板元素的功能，并为每个面板添加点击事件监听器，同时新增模块化函数以移除面板的激活状态，提升代码的可维护性和用户交互体验。

--- html5/panel/common.js
+++ html5/panel/common.js
@@ -1,4 +1,7 @@
+// 获取所有面板元素
 const panels = document.querySelectorAll('.qq-panel');
+
+// 为每个面板添加点击事件监听器
 panels.forEach(panel => {
     // JS 是事件机制的语言
     // 每一个元素上监听
@@ -9,6 +12,10 @@ panels.forEach(panel => {
     })
 })
 
+/**
+ * 移除所有面板的激活状态
+ * 这是一个模块化的函数，用于重置所有面板的状态
+ */
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('qq-panel_active');
