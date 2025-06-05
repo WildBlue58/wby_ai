f413e8f6f777ad46d2c6485b80da9725b3ed7f8c
WildBlue <2398205893@qq.com>
2025-06-05 21:28:24 +0800

    在app.js中新增注释，详细说明HTTP请求的结构、角色定义及请求处理过程，提升代码可读性和维护性。

--- deepseek/webllm/js/app.js
+++ deepseek/webllm/js/app.js
@@ -18,7 +18,13 @@
 // Chat 方式 AIGC 生成/完成 返回的内容
 // 由OpenAI 制定的
 // 请求行
-const endpoint = "https://api.deepseek.com/chat/completions"
+// 命名
+// WebLLM Web 底层是 http 协议
+// LLM API 服务
+// api.deepseek.com 二级域名 LLM服务以API的方式提供
+// https 加密的http 更安全
+// /chat 聊天的方式 messages 
+const endpoint = "https://api.deepseek.com/chat/completions" //deepseek_url
 // 请求头
 const headers = {
     // 内容类型
@@ -30,6 +36,10 @@ const headers = {
 const payload = {
     model: "deepseek-chat",
     messages: [
+        // Chat 三种方式
+        // 1. 系统角色 只会出现一次 设置系统的角色 开始会话时
+        // 2. 用户角色 user 提问
+        // 3. 助手角色
         {
             role: "system",
             content: "You are a helpful assistant."
@@ -45,8 +55,13 @@ const payload = {
 fetch(endpoint, {
     method: "POST",// 请求方法
     headers: headers,// 请求头
+    // http 请求传输只能是字符串，二进制流
     body: JSON.stringify(payload)// 请求体
+    // 请求 + LLM 生成需要花时间
+    // http 是基于请求相应的简单协议
+    // 返回的也是文本或二进制流
 }).then(res => res.json())
+    // 解析返回的json 数据 也要花时间
 .then(data => {
     console.log(data);
     document.querySelector('#reply').innerHTML +=
