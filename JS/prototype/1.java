2a48ec7dc9fea0a39320b14d2479beea677b94d9
WildBlue <2398205893@qq.com>
2025-06-06 17:02:29 +0800

    新增JS/prototype/1.java文件，定义了Person类的示例代码，展示了构造方法和成员方法，提升代码示例的可读性和学习价值。

---
+++ JS/prototype/1.java
@@ -0,0 +1,16 @@
+// package prototype;
+// // 定义 Person 类
+// public class Person {
+//     // 成员变量
+//     private int pupyAge;
+//     // 构造方法
+//     public Person(int pupyAge) {
+//         this.pupyAge = pupyAge;
+//     }
+//     // 共有成员方法
+//     public void say() {
+//         System.out.println("汪汪汪");
+//     }
+// }
+
+// 这个 1.java 只作示例，其不符合.java的很多基本创建需求，补药被误导
