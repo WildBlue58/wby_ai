e9a5acdedad5cdb8074441cd0831e26fda2a4d55
WildBlue <2398205893@qq.com>
2025-06-07 15:52:41 +0800

    更新Todos组件，添加从父组件接收待办事项的功能，并渲染待办事项列表。

--- React/todoListComponents/src/components/Todos.jsx
+++ React/todoListComponents/src/components/Todos.jsx
@@ -1,6 +1,15 @@
-function Todos() { 
+// 列表的渲染
+function Todos(props) { 
+    console.log(props, '/////')
+    const todos = props.todos
     return (
+        // 父组件传过来的数据状态呢?传参
         <ul>
+            {
+                todos.map(todo => (
+                    <li key={ todo.id }>{ todo.text }</li>
+                ))
+            }
         </ul>
     )
 }
