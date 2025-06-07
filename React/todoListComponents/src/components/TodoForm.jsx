4a9b4c68c370a2f967c84dc9ac7b930d8306b12b
WildBlue <2398205893@qq.com>
2025-06-07 17:09:06 +0800

    更新TodoForm组件，添加表单提交处理和输入状态管理功能，优化用户交互体验。

--- React/todoListComponents/src/components/TodoForm.jsx
+++ React/todoListComponents/src/components/TodoForm.jsx
@@ -1,6 +1,30 @@
-function TodoForm() { 
+import { useState } from 'react'
+function TodoForm(props) { 
+    const onAdd = props.onAdd
+    const [text, setText] = useState('')
+
+    const handleSubmit = (e) => {
+        // 阻止默认行为
+        // 由 JS 来控制
+        e.preventDefault() // Event api
+
+        console.log(e.target.value);
+        onAdd(text)
+        // todos? 打报告
+    }
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
     return (
-        <form>
+        <form action="http://www.baidu.com" onSubmit={ handleSubmit }>
+            <input
+                type="text"
+                placeholder="请输入待办事项"
+                value={text}
+                onChange={handleChange}
+            />
+            <button type="submit">添加</button>
         </form>
     )
 }
