91af13bb99a28dcf3498aee86e031eee50668ae6
WildBlue <2398205893@qq.com>
2025-06-07 11:14:16 +0800

    新增React/todoListComponents/src/App.jsx文件，定义应用的主组件结构，包含两个TodoList组件，搭建基本的应用框架。

---
+++ React/todoListComponents/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import TodoList from './components/TodoList'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div></div>
+      <TodoList />
+      <TodoList />
+    </>
+  )
+}
+
+export default App
