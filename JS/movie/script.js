2ba701140adf3178b1666849a70fb757c967ce91
WildBlue <2398205893@qq.com>
2025-06-09 20:57:52 +0800

    新增script.js文件，实现电影数据获取和搜索功能，优化用户交互体验。

---
+++ JS/movie/script.js
@@ -0,0 +1,60 @@
+// 配置
+// 电影接口地址
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+// DOM 编程 原生JS
+// 返回的DOM 节点对象
+// Object.prototype.toString.call(oForm) 判断具体类型
+const oForm = document.querySelector('#form');
+const oInput = document.querySelector('#search');
+console.log(oForm);
+
+// 获取电影
+const getMovies = (keyword) => {
+    // console.log(keyword);
+    let reqUrl = ''
+    if (keyword) {
+        // 搜索
+        reqUrl = SEARCH_API + keyword
+    } else {
+        // 获取电影
+        reqUrl = API_URL
+    }
+    // 发送请求
+    fetch(reqUrl)
+         // 二进制流
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            showMovies(data.results);
+        })
+}
+
+// Movie List Render 电影列表渲染
+const showMovies = (movies) => {
+    console.log(movies);
+}
+
+// 页面加载完成后执行
+window.onload = function () {
+    getMovies(API_URL);
+}
+
+oForm.addEventListener('submit', function (event) {
+    // 事件对象
+    console.log(event,'////');
+    event.preventDefault();// 阻止默认行为
+    const search = oInput.value.trim();
+    if (search) {
+        // 搜索电影(模块化)
+        getMovies(search);
+    }
+    // console.log(search);
+    // if (search.trim()) {// 登录栏 输入框 输入内容 有空格 需要trim() 去掉空格
+    //     console.log(search.trim());
+    // } else {
+    //     console.log('请输入search');
+    // }
+})
+    
