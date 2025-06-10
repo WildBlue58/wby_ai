385b3530405efcdd280ea00e1f14aa7cc326fb34
WildBlue <2398205893@qq.com>
2025-06-10 21:30:57 +0800

    新增音频生成功能，集成火山引擎TTS配置，提升用户交互体验和音频播放能力。

--- deepseek/tts/React-music/src/App.jsx
+++ deepseek/tts/React-music/src/App.jsx
@@ -2,19 +2,57 @@ import { useState,useRef } from 'react'
 import './App.css'
 
 function App() {
+  // 火山引擎tts 配置文件
+  const TOKEN = 'OgyyBXBZTFFq8Ygsh9GoPcC1qVkHJ56Z'
+  const APP_ID = '8258845810'
+  const CLUSTER_ID = 'volcano_tts'
+
+  // 代码可读性高于一切
+  const [prompt, setPrompt] = useState('大家好，我是练习时长两年半的偶像练习生')
+  
   // React Use 开头 ref hook 可以获取 DOM 元素
   const audioPlayer = useRef(null)
-  console.log(audioPlayer,'/////');
+  console.log(audioPlayer, '/////');
+  
   const playMusic = () => {
     // console.log(audioPlayer,'+++');
     console.log('Play Music');
     audioPlayer.current.play();
   }
+
+  const generateAudio = () => {
+    // 女性
+    // const voiceName = 'zh_female_shuangkuaisisi_moon_bigtts'
+    const voiceName = 'zh_male_sunwukong_mars_bigtts'
+    const endpoint = '/tts/api/v1/tts' // tts api llm 服务接口地址
+
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization : `Bearer ${TOKEN}`
+    }
+
+    const body = {
+      voice_name: voiceName,
+      text: prompt,
+    }
+   }
+  
   return (
-    <>
+    <div className='container'>
+      <div>
+        <label>Prompt</label>
+        <button onClick={generateAudio}>生成并播放</button>
+        <textarea
+          className='input'
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        >
+
+        </textarea>
+      </div>
       <audio ref={audioPlayer} src="/sounds/snare.wav"></audio>
-      <button onClick={playMusic}>播放</button>
-    </>
+      {/* <button onClick={playMusic}>播放</button> */}
+    </div>
   )
 }
 
