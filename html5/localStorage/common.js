6604164e0620d1f2138d114cca2aadd243bbd87c
WildBlue <2398205893@qq.com>
2025-06-19 16:15:05 +0800

    新增common.js文件，添加本地存储功能，支持添加和展示项目列表，提升项目的交互性和用户体验。

--- html5/localStorage/common.js
+++ html5/localStorage/common.js
@@ -1,9 +1,33 @@
 const addItems = document.querySelector('.add-items')// form
 const itemsList = document.querySelector('.plates')// 列表
+let items = [];
 
-function addItem() {
+function addItem(e) {
     e.preventDefault();
-    
+    const text = this.querySelector('[name=item]').value.trim();
+    if (!text) return;
+    const item = {
+        text,
+        done: false
+    };
+    items.push(item);
+    populateList(items, itemsList);
+    this.reset();
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates
+        .map((plate, i) => {
+            return `
+                <li>
+                    <label>
+                        <input type="checkbox" data-index="${i}" ${plate.done ? 'checked' : ''} />
+                        ${plate.text}
+                    </label>
+                </li>
+            `;
+        })
+        .join('');
 }
 
 addItems.addEventListener('submit', addItem)
