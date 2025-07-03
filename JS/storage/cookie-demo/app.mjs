d6dcb97903c5ab040d6426300868433b6e48f228
WildBlue <2398205893@qq.com>
2025-07-03 15:39:55 +0800

    在app.mjs文件中新增ES6模块化相关注释，说明Node.js对ES6模块的支持情况，提升代码可读性和学习参考价值。

--- JS/storage/cookie-demo/app.mjs
+++ JS/storage/cookie-demo/app.mjs
@@ -1,3 +1,10 @@
+// ES6 模块化
+// mjs 后缀使用ES6模块化
+// 模块化是语言的能力
+// node 默认不支持ES6 模块化
+// node 最新版本支持了 22
+// node 准备跟require common.js say goodbye
+// ES6 module 更先进 mjs 
 import http from 'http';
 
 const server = http.createServer((req, res) => {
