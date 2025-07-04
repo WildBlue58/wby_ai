ed36b60a3a9bacc67cc024c0c3d7bbaf05f8309c
WildBlue <2398205893@qq.com>
2025-07-04 20:25:25 +0800

    新增PictureCard组件，定义图片卡片的基本结构，提升项目的组件化和可复用性。

---
+++ React/shotword/src/components/PictureCard/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+const PictureCard = (props) => { 
+    const {
+        word,
+        audio,
+        uploadImg,
+    } = props
+    return (
+        <div>
+        PictureCard
+        </div>
+    )
+}
+
+export default PictureCard;
