f4352f8a70070be69560223b3def9a4d461b33ae
WildBlue <2398205893@qq.com>
2025-07-05 22:06:37 +0800

    新增Timer组件，使用useEffect和useState实现定时器功能，提升应用的交互性和实时性。

---
+++ React/hooks/effect-demo/src/components/Timer/index.jsx
@@ -0,0 +1,27 @@
+import {
+    useEffect,
+    useState,
+} from 'react'
+const Timer = () => { 
+    const [time, setTime] = useState(0);
+
+    console.log('组件函数执行');
+    console.log("JSX 编译");
+    useEffect(() => {
+        console.log('组件渲染完成');
+        const interval = setInterval(() => {
+            setTime(prevTime => prevTime + 1);
+        }, 1000);
+        return () => {
+            console.log('组件卸载');
+            clearInterval(interval);
+        }
+    }, [])
+    return (
+        <div>
+            已经运行{time}秒
+        </div>
+    )
+}
+
+export default Timer;
