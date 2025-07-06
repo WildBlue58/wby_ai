46b7d4a424740fdcc54e13f23205153fefa37f37
WildBlue <2398205893@qq.com>
2025-07-06 12:07:18 +0800

    优化PictureCard组件，调整props顺序，新增播放音频功能，提升用户交互体验。

--- React/shotword/src/components/PictureCard/index.jsx
+++ React/shotword/src/components/PictureCard/index.jsx
@@ -1,47 +1,53 @@
 import {
-    useState,
-} from 'react'
-import './style.css'
+  useState
+} from 'react';
+
+import './style.css';
 
 const PictureCard = (props) => {
-    const {
-        audio,
-        word,
-        uploadImg,
-    } = props
+  const {
+    word,
+    audio,
+    uploadImg
+  } = props
+  const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png')
+  const uploadImgData = (e) => {
+    const file = (e.target).files?.[0];
+    if (!file) { return; }
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            const data = reader.result;
+            setImgPreview(data);
+            uploadImg(data);
+            resolve(data);
+        }
+        reader.onerror = (error) => { reject(error); };
+    })
+  }
 
-    const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png')
-    const uploadImgData = (e) => {
-        // 可选链操作符
-        const file = (e.target).files?.[0];
-        if (!file) { return; }
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => {
-                const data = reader.result;
-                setImgPreview(data);
-                uploadImg(data);
-                resolve(data);
-            }
-            reader.onerror = (error) => { reject(error); };
-        })
-    }
-
-    return (
-        <div className="card">
-            <input
-                type="file"
-                id="selectImage"
-                accept=".jpg,.jpeg,.png,.gif"
-                onChange={uploadImgData}
-            />
-            <label htmlFor="selectImage" className="upload">
-                <img src={imgPreview} alt="preview" ></img>
-            </label>
-            <div className="word">{word}</div>
-        </div>
-    )
+  const playAudio = () => {
+    const audioEle = new Audio(audio);
+    audioEle.play();
+  }
+  return (
+    <div className="card">
+      <input 
+        id="selectImage" 
+        type="file" 
+        accept=".jpg,.jpeg,.png,.gif" 
+        onChange={uploadImgData}
+      />
+      <label htmlFor="selectImage" className="upload">
+        <img src={imgPreview} alt="preview" />
+      </label>
+      <div className="word">{word}</div>
+      <div className="playAudio" onClick={audio ? playAudio : () => alert('请先上传图片并生成语音')}>
+        <img width="20px" src="https://res.bearbobo.com/resource/upload/Omq2HFs8/playA-3iob5qyckpa.png" alt="logo" />
+      </div>
+    </div>
+  )
 }
 
 export default PictureCard;
