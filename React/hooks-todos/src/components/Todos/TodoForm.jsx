d7991f84e442ceabe0c623c3421d321335a5c7dd
WildBlue <2398205893@qq.com>
2025-07-07 21:58:21 +0800

    更新TodoForm组件，添加标题和表单结构，优化组件的可读性和功能性。

--- React/hooks-todos/src/components/Todos/TodoForm.jsx
+++ React/hooks-todos/src/components/Todos/TodoForm.jsx
@@ -3,9 +3,13 @@ import {
 } from 'react'
 
 const TodoForm = () => { 
+    // JSX 一定得有唯一的最外层元素 树来编译解析JSX,
     return (
         <>
-        TodoForm
+            <h1 className="header">TodoList</h1>
+            <form>
+
+            </form>
         </>
     )
 }
