83193dbb15e27231e974947c00b2fa265eb10f69
WildBlue <2398205893@qq.com>
2025-07-07 20:51:56 +0800

    新增App.jsx文件，定义应用的主组件结构，包含Todos组件，提升项目的功能性和可维护性。

---
+++ React/hooks-todos/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      {/* 开发的任务单位就是组件 */}
+    <Todos />
+    </>
+  )
+}
+
+export default App
