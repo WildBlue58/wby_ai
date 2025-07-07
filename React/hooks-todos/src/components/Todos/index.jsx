c1d48e9224e5252da165b23220c74de6a6c4a19c
WildBlue <2398205893@qq.com>
2025-07-07 20:52:59 +0800

    新增Todos组件，管理待办事项的状态和数据流，提升项目的功能性和可维护性。

---
+++ React/hooks-todos/src/components/Todos/index.jsx
@@ -0,0 +1,42 @@
+import { 
+    // 响应式状态hooks
+    useState // react 函数式编程 好用的以 use 开头的函数
+} from 'react'
+
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+
+const Todos = () => {
+    // 数据流管理
+    // 父组件持有管理数据 props 传递数据 子组件通过 props 自定义函数
+    // 通知父组件
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: '学习react',
+            isCompleted: false
+        },
+        {
+            id: 2,
+            title: '学习vue',
+            isCompleted: false
+        },
+    ])
+
+    // 新增todo
+    const addTodo = (todo) => {
+        // setTodo
+        setTodos([...todos, todo])
+    }
+    
+    return (
+        <div className="app">
+            Todos
+            {/* 自定义事件 */}
+            <TodoForm onAddTodo={addTodo}/>
+            <TodoList todos={todos}/>
+        </div>
+    )
+}
+
+export default Todos
