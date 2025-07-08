a6d33639854fe417c39b753649a353c459042c62
WildBlue <2398205893@qq.com>
2025-07-08 21:36:29 +0800

    更新TodoList组件，重构为接收props，新增待办事项的显示、切换和删除功能，提升组件的交互性和用户体验。

--- React/hooks-todos/src/components/Todos/TodoList.jsx
+++ React/hooks-todos/src/components/Todos/TodoList.jsx
@@ -1,10 +1,30 @@
 import TodoItem from './TodoItem'
 
-const TodoList = () => { 
+const TodoList = (props) => { 
+    const {
+        todos,
+        onToggle,
+        onDelete,
+    } = props
+
     return (
-        <>
-            TodoList
-        </>
+        <ul className="todo-list">
+            {/* TodoList */}
+            {todos.length > 0 ? (
+                <div >
+                    {todos.map(todo => (
+                        <TodoItem
+                            key={todo.id}
+                            {...todo}
+                            onToggle={() => onToggle(todo.id)}
+                            onDelete={() => onDelete(todo.id)}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <p>暂无待办事项</p>
+            )}
+        </ul>
     )
 }
 
