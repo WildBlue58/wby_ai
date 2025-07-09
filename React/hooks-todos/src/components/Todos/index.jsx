ca39fda535ae7acb9d82abe9c70eb6ed609d3853
WildBlue <2398205893@qq.com>
2025-07-09 21:26:53 +0800

    重构Todos组件，使用自定义hooks管理todos状态，简化代码逻辑，提高可读性。

--- React/hooks-todos/src/components/Todos/index.jsx
+++ React/hooks-todos/src/components/Todos/index.jsx
@@ -1,61 +1,26 @@
 import { 
     // 响应式状态hooks
-    useState // react 函数式编程 好用的以 use 开头的函数
+    useState, // react 函数式编程 好用的以 use 开头的函数
+    useEffect
 } from 'react'
 
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import TodoItem from './TodoItem'
+import { useTodos } from '@/hooks/userTodos'
 
 const Todos = () => {
     // 数据流管理
     // 父组件持有管理数据 props 传递数据 子组件通过 props 自定义函数
     // 通知父组件
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: '学习react',
-            isCompleted: false
-        },
-        {
-            id: 2,
-            text: '学习vue',
-            isCompleted: false
-        },
-    ])
+    const {
+        todos,
+        addTodo,
+        onToggle,
+        onDelete
+    } = useTodos()
 
-    // 新增todo
-    const addTodo = (text) => {
-        // setTodo
-        // setTodos([...todos, todo])
-        // 数据状态是对象的时候
-        setTodos([
-            ...todos,
-            {
-                id: Date.now(), // 时间戳
-                text: text,
-                isCompleted: false
-            }
-        ])
-    }
-
-    const onToggle = (id) => {
-        console.log(id);
-        // todos 数组找到id 为 id，isCompleted !isCompleted
-        // 响应式? 返回一个全新的todos map
-        // state 是对象或数组的时候
-        
-        setTodos(todos.map(
-            todo => todo.id === id
-            ? { ...todo, isCompleted: !todo.isCompleted }
-            : todo
-        ))
-    }
-
-    const onDelete = (id) => {
-        setTodos(todos.filter(todo => todo.id !== id))// 过滤器 返回一个全新的todos
-    }
-    
+ 
     return (
         <div className="app">
             {/* 自定义事件 */}
