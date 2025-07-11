10d0775725092788bc26a0c283a4a715f85a1fe0
WildBlue <2398205893@qq.com>
2025-07-11 21:32:53 +0800

    完善App.jsx文件，添加前端路由配置，支持Home和About页面的导航功能。

--- React/router-demo/src/App.jsx
+++ React/router-demo/src/App.jsx
@@ -1,20 +1,27 @@
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom'
+  BrowserRouter as Router, // 前端路由
+  Routes, // 路由设置容器
+  Route, // 单条
+  Link,
+} from "react-router-dom";
 
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-
   return (
     <>
-     
+      {/* 前端路由接管一切,配置 */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
