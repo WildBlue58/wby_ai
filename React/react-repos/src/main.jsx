e7b997dec50612fa3141517f110a67b5c8d09f18
WildBlue <2398205893@qq.com>
2025-07-18 19:58:47 +0800

    优化main.jsx文件，调整导入顺序，统一引号风格，提升代码可读性。

--- React/react-repos/src/main.jsx
+++ React/react-repos/src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { GlobalProvider } from './context/GlobalContext'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalContext";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 // 页面级别组件由路由驱动
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <GlobalProvider>
     <Router>
       <App />
-    </Router>  
+    </Router>
   </GlobalProvider>
-)
+);
