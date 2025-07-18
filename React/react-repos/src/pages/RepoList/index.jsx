50efa42262c21612cba6183170c5afc5d957d5af
WildBlue <2398205893@qq.com>
2025-07-18 19:59:05 +0800

    实现RepoList组件，添加参数解析、数据加载和错误处理逻辑，优化用户导航体验。

--- React/react-repos/src/pages/RepoList/index.jsx
+++ React/react-repos/src/pages/RepoList/index.jsx
@@ -1,9 +1,35 @@
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useRepos } from "@/hooks/useRepos";
+
 const RepoList = () => {
-    return (
-        <>
-        RepoList
-        </>
-    )
+  const { id } = useParams();
+  console.log(useParams());
+  const navigate = useNavigate();
+  // hooks
+  const { repos, loading, error } = useRepos(id);
+  console.log(repos, loading, error);
+  useEffect(() => {
+    if (!id.trim()) {
+      navigate("/");
+    }
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <>
+      <h2>Repositories for {id}</h2>
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link to={`/users/${id}/repos/${repo.id}`}>{repo.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default RepoList;
