6ef61a01b23427481eff26d78e0d402836595465
WildBlue <2398205893@qq.com>
2025-07-19 16:22:36 +0800

    新增2.jsx文件，定义React元素和列表，展示JSX语法的使用，进一步丰富示例内容。

---
+++ React/interview/compile_jsx/2.jsx
@@ -0,0 +1,8 @@
+const element = /*#__PURE__*/React.createElement("h1", null, "\uD83E\uDD70Love Xiang\u2764\uFE0F");
+const element2 = /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", {
+  key: "wby1"
+}, "1"), /*#__PURE__*/React.createElement("li", {
+  key: "dgx2"
+}, "2"), /*#__PURE__*/React.createElement("li", {
+  key: "love"
+}, "3"));
