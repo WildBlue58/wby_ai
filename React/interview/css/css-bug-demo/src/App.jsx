54a7a8443b42cee98f1292fc72c22dc070d25e59
WildBlue <2398205893@qq.com>
2025-07-21 17:03:06 +0800

    新增App.jsx文件，定义了应用的基本结构，包含两个按钮组件，作为项目的主要界面。

---
+++ React/interview/css/css-bug-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "./components/Button.jsx";
+import AnotherButton from "./components/AnotherButton.jsx";
+// import 运行 路由懒加载
+
+function App() {
+  return (
+    <>
+      <Button />
+      <AnotherButton />
+    </>
+  );
+}
+
+export default App;
