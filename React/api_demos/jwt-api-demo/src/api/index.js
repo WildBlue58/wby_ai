0700de01173655042c47410ee458ce584291b447
WildBlue <2398205893@qq.com>
2025-07-21 21:16:39 +0800

    新增index.js文件，定义todos和repos接口的API请求方法，提升项目的数据获取能力和功能性。

---
+++ React/api_demos/jwt-api-demo/src/api/index.js
@@ -0,0 +1,11 @@
+import axios from "./config";
+
+// todos接口
+export const getTodos = () => {
+  return axios.get("/api/todos");
+};
+
+// repos接口
+export const getRepos = () => {
+  return axios.get("/repos");
+};
