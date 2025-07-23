ac4f9510e91a8886b72124df802f18d565119021
WildBlue <2398205893@qq.com>
2025-07-23 17:00:52 +0800

    新增RequireAuth组件，实现用户登录状态的验证，确保未登录用户无法访问受保护的路由，提升应用的安全性和用户体验。

---
+++ React/jwt-demo/src/components/RequireAuth/index.jsx
@@ -0,0 +1,20 @@
+import { useUserStore } from "../../store/user";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const RequireAuth = ({ children }) => {
+  const { isLogin } = useUserStore();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (!isLogin) {
+      navigate("/login", { from: pathname });
+    }
+  }, [isLogin, navigate, pathname]);
+  if (!isLogin) {
+    return null;
+  }
+  return <>{children}</>;
+};
+
+export default RequireAuth;
