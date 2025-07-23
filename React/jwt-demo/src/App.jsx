276616513d14749112c4740b3e50ea0eaca28965
WildBlue <2398205893@qq.com>
2025-07-23 17:00:11 +0800

    重构App组件，新增路由管理和懒加载功能，优化用户界面结构，提升应用性能和用户体验。

--- React/jwt-demo/src/App.jsx
+++ React/jwt-demo/src/App.jsx
@@ -1,20 +1,39 @@
-import { useState, useEffect } from 'react'
-import { getUser } from './api/user'
-import './App.css'
+import { useState, useEffect, lazy, Suspense } from "react";
+import { getUser } from "./api/user";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const Home = lazy(() => import("./views/Home"));
+const Pay = lazy(() => import("./views/Pay"));
+const Login = lazy(() => import("./views/Login"));
+const RequireAuth = lazy(() => import("./components/RequireAuth"));
 
 function App() {
   useEffect(() => {
     (async () => {
-      const res = await getUser()
+      const res = await getUser();
       console.log(res, "----res");
-    })()
+    })();
   }, []);
-  
+
   return (
     <>
-   
+      <NavBar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/pay" element={
+            <RequireAuth>
+              <Pay />
+            </RequireAuth>
+          } />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
