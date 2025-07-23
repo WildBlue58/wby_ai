1a655fd8321da78704223aabd6f850915c461a8e
WildBlue <2398205893@qq.com>
2025-07-23 14:27:09 +0800

    新增JWT登录功能，完善用户身份验证和信息获取接口，提升安全性和用户体验。

--- React/jwt-demo/mock/login.js
+++ React/jwt-demo/mock/login.js
@@ -1,3 +1,9 @@
+import jwt from "jsonwebtoken";
+
+// 安全性 编码的时候加密
+// 解码的时候用于解密
+const secret = "!$123asdfg";
+
 // login 模块 mock
 export default [
   {
@@ -7,10 +13,58 @@ export default [
     response: (req, res) => {
       // req, username, password
       const { username, password } = req.body;
+      if (username !== "admin" || password !== "123456") {
+        return {
+          code: 1,
+          message: "用户名或密码错误",
+        };
+      }
+      // json 用户数据
+      const token = jwt.sign(
+        {
+          user: {
+            id: "001",
+            username: "admin",
+            level: 4,
+          },
+        },
+        secret,
+        {
+          expiresIn: "1h", // 1小时后过期
+        }
+      );
+      console.log(token, "----token");
+      // 生成token 颁发令牌
       return {
+        token,
         username,
         password,
       };
     },
   },
+  {
+    url: "/api/user",
+    method: "get",
+    response: (req, res) => {
+      // 用户端 token headers 
+      const token = req.headers["authorization"];
+      try {
+        const decode = jwt.decode(token, secret)
+        console.log(decode, "----decode");
+        return {
+          code: 0,
+          data: decode.user,
+          message: "获取用户信息成功",
+        }
+      } catch (err) {
+        return {
+          code: 1,
+          message: "Invalid token",
+        }
+       }
+      return {
+        token
+      }
+    },
+  }
 ];
