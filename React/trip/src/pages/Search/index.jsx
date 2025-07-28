485df85e2466d8f9560b8d14acfcb9dedffcafed
WildBlue <2398205893@qq.com>
2025-07-28 18:50:22 +0800

    新增SearchBox组件样式文件，并在Search页面中集成SearchBox，优化搜索功能的交互体验。

--- React/trip/src/pages/Search/index.jsx
+++ React/trip/src/pages/Search/index.jsx
@@ -1,5 +1,17 @@
+import SearchBox from "@/components/SearchBox";
+
 const Search = () => {
-  return <div>Search</div>;
+  // api 请求
+  // 单向数据流
+  // 反复生成 useCallback
+  const handleQuery = (keyword) => {
+    console.log(keyword);
+  };
+  return (
+    <div>
+      <SearchBox onQuery={handleQuery} />
+    </div>
+  );
 };
 
 export default Search;
