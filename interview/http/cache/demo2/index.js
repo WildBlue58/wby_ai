9c4474ae1947b0f47076a40901b533ce7afcd059
WildBlue <2398205893@qq.com>
2025-08-24 14:27:14 +0800

    新增HTTP缓存示例，包括服务器实现、缓存策略及相关HTML和JavaScript文件，提升项目的学习价值和实用性。

--- interview/http/cache/demo2/index.js
+++ interview/http/cache/demo2/index.js
@@ -9,29 +9,37 @@ function md5(data) {
   return crypto.createHash("md5").update(data).digest("hex");
 }
 
-http.createServer((req, res) => {
-  if (req.url === "/") {
-    const html = fs.readFileSync("test-html", "utf-8");
-    Response.writeHead(200, {
-      "Content-Type": "text/html",
-      "Cache-Control": "max-age=10,public",
-      ETag: md5(html),
-    });
-    res.end(html);
-  }
-  if (req.url === "/script.js") {
-    // 浏览器缓存文件的 hash
-    const noneMatch = req.headers["if-none-match"];
-    const filePath = path.join(__dirname, "script.js");
-    const buffer = fs.readFileSync(filePath);
-    const fileMd5 = md5(buffer);
-    if (noneMatch === fileMd5) {
-      res.writeHead(304, {
-        "Content-Type": "text/javascript",
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      const html = fs.readFileSync(path.join(__dirname, "test-html"), "utf-8");
+      res.writeHead(200, {
+        "Content-Type": "text/html",
         "Cache-Control": "max-age=10,public",
         ETag: md5(html),
       });
-      res.end();
+      res.end(html);
     }
-  }
-});
+    if (req.url === "/script.js") {
+      // 浏览器缓存文件的 hash
+      const noneMatch = req.headers["if-none-match"];
+      const filePath = path.join(__dirname, "script.js");
+      const buffer = fs.readFileSync(filePath);
+      const fileMd5 = md5(buffer);
+
+      if (noneMatch === fileMd5) {
+        res.statusCode = 304; // 304 Not Modified
+        res.end();
+        return;
+      }
+      res.writeHead(200, {
+        "Content-Type": "text/javascript",
+        "Cache-Control": "max-age=10,public",
+        ETag: fileMd5,
+      });
+
+      const readStream = fs.createReadStream(filePath);
+      readStream.pipe(res);
+    }
+  })
+  .listen(1234);
