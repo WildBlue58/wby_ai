29f3e16bee98581772f348eb9d1879584f566b70
WildBlue <2398205893@qq.com>
2025-08-25 21:34:31 +0800

    新增Echarts示例项目结构，包括基本配置文件、依赖管理、样式设置及示例代码，支持饼状图展示，同时更新README.md以提供项目使用说明，提升项目的可用性和学习价值。

---
+++ interview/visual/echarts-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Demo from "./components/Demo";
+
+function App() {
+  return (
+    <>
+      <Demo />
+    </>
+  );
+}
+
+export default App;
