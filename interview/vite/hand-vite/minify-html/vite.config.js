b637d818143a351ccefea931293942917543a3aa
WildBlue <2398205893@qq.com>
2025-10-02 21:08:34 +0800

    新增Vite + React项目模板，包含基本的HTML结构、CSS样式、ESLint配置及HTML压缩插件。更新README.md以介绍项目功能和使用方法，提升文档的可读性和实用性。

---
+++ interview/vite/hand-vite/minify-html/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import htmlMinifyPlugin from "./plugins/vite-plugin-html-minify";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), htmlMinifyPlugin()],
+});
