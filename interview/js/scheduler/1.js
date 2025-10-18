9b929367a41a7d9f3fe747b6131bfea4e6a0775b
WildBlue <2398205893@qq.com>
2025-10-18 23:28:14 +0800

    新增任务调度器TaskScheduler的实现，支持最大并发任务数设置，并提供addTask方法以自动调度执行Promise异步任务，同时新增README文档详细说明功能和使用方法，提升代码的可读性和文档的实用性。

---
+++ interview/js/scheduler/1.js
@@ -0,0 +1,70 @@
+class TaskScheduler {
+  constructor(maxConcurrency = 2) {
+    this.maxConcurrency = maxConcurrency;
+    this.runningCount = 0; // 正在执行的任务数量
+    this.taskQueue = []; // FIFO
+  }
+  addTask(task) {
+    return new Promise((resolve, reject) => {
+      const run = () => {
+        this.runningCount++;
+        task()
+          .then(resolve)
+          .catch(reject)
+          .finally(() => {
+            this.runningCount--;
+            this.schedule();
+          });
+      };
+      this.taskQueue.push(run);
+      this.schedule();
+    });
+  }
+  schedule() {
+    while (
+      this.runningCount < this.maxConcurrency &&
+      this.taskQueue.length > 0
+    ) {
+      const task = this.taskQueue.shift();
+      task();
+    }
+  }
+}
+
+const tasks1 = () =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("任务1完成");
+      resolve(1);
+    }, 1000)
+  );
+
+const tasks2 = () =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("任务2完成");
+      resolve(2);
+    }, 2000)
+  );
+
+const tasks3 = () =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("任务3完成");
+      resolve(3);
+    }, 1500)
+  );
+
+const tasks4 = () =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("任务4完成");
+      resolve(4);
+    }, 1000)
+  );
+
+const scheduler = new TaskScheduler();
+scheduler.addTask(tasks1);
+scheduler.addTask(tasks2);
+scheduler.addTask(tasks3);
+scheduler.addTask(tasks4);
